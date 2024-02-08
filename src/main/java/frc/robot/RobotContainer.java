f52897407cb3bc6ac64101e0322f5e0f1fb32ba9
Gabriel Magwood <magwoodgabe@gmail.com>
2024-02-07 20:51:35 -0500

    Vision Testing | VisionHandler Command for Testing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,7 @@ import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.commands.VisionHandler;
 import frc.robot.subsystems.vision.AprilTagIO;
 import frc.robot.subsystems.vision.AprilTagIOPhotonVision;
 import frc.robot.subsystems.vision.NoteDetectionIOPhotonVision;
@@ -19,13 +19,14 @@ import frc.robot.subsystems.vision.Vision;
 
 public class RobotContainer {
   private final Vision vision = new Vision(new AprilTagIO[] {new AprilTagIOPhotonVision("CameraFront", new Transform3d(0, 0, 0, new Rotation3d(0, 0, 0)))} , new NoteDetectionIOPhotonVision("Poly"));
-
+  private final Command visionHandler = new VisionHandler(vision);
 
   private final LoggedDashboardChooser<Command> autonChooser = new LoggedDashboardChooser<>("Pick Auton");
   public RobotContainer() {
-    vision.setDefaultCommand(new RunCommand( () -> vision.getVisionOdometry()));
+    vision.setDefaultCommand(visionHandler);
     autonChooser.addDefaultOption("Do Nothing", new InstantCommand());
     configureBindings();
+
   }
 
   private void configureBindings() {}
