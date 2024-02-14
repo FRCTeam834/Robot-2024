1bf74d3efcf8d58af93d3f776c880c886331a90b
“Real <real.tronsi@gmail.com>
2024-02-14 18:22:16 -0500

    replace pathplanner's dogwater autochooser builder

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,7 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.util.PathPlannerLogging;
 
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.DriveWithSpeeds;
@@ -73,7 +74,10 @@ public class RobotContainer {
      * Pathplanner stuff
      */
     swerve.configureAutoBuilder(poseEstimator);
-    autoChooser = AutoBuilder.buildAutoChooser();
+    autoChooser = new SendableChooser<>();
+    autoChooser.setDefaultOption("Do Nothing", new InstantCommand());
+    autoChooser.addOption("S-Curve", new PathPlannerAuto("S-Curve"));
+    
     SmartDashboard.putData(autoChooser);
     pathPlannerField = new Field2d();
     SmartDashboard.putData("PathTarget", pathPlannerField); 
